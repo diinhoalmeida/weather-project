26e3f2bae8779e6bebb15e5ed892de908951dd58
Edilson Almeida <edifilho1409@outook.com>
2023-07-30 13:45:37 -0300

    new: Card Weather Complete
    Card de previsao do tempo completo, todos itens typados e no meio do processo foi feito uma refatoração em SOLID para melhoria de codigo

--- src/components/CardWeather/components/Location/location.tsx
+++ src/components/CardWeather/components/Location/location.tsx
@@ -1,25 +1,43 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { AiOutlineClose } from "react-icons/ai";
 
 interface LocationProps {
   abreviationState: string;
   countryName: string;
   cityName: string;
+  temperature: number;
+  setOpenCardWeather: (arg: boolean) => void;
+  condition: string;
 }
 
 const Location = ({
   abreviationState,
   countryName,
+  temperature,
+  condition,
+  setOpenCardWeather,
   cityName,
 }: LocationProps) => {
   return (
-    <Flex w="full" alignItems="center" justifyContent="space-between" mb="10px">
-      <Text fontWeight="semibold">
-        {`${cityName}, ${abreviationState} - ${countryName}`}
+    <Flex flexDirection="column">
+      <Flex w="full" alignItems="center" mb="10px" position="relative">
+        <Text fontWeight="semibold">
+          {`${cityName}, ${abreviationState} - ${countryName}`}
+        </Text>
+        <Box
+          position="absolute"
+          right={{ base: -12, sm: 0 }}
+          top={{ base: -7, sm: 0 }}
+          onClick={() => setOpenCardWeather(false)}
+        >
+          <AiOutlineClose
+            style={{ color: "#FF9A00", fontSize: "20px", cursor: "pointer" }}
+          />
+        </Box>
+      </Flex>
+      <Text fontWeight="semibold" mb="10px" fontSize="3xl">
+        {`${Math.floor(temperature)}ºC ${condition}`}
       </Text>
-      <AiOutlineClose
-        style={{ color: "#FF9A00", fontSize: "20px", cursor: "pointer" }}
-      />
     </Flex>
   );
 };
